f278bd11e9280a4b47e781305037e2ba80e2aad9
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-01-01 17:30:11 +0100

    [STABLE] Work in progress on VDE. Added temporal window counting method.

--- VDE/velocityDensityEstimator.cpp
+++ VDE/velocityDensityEstimator.cpp
@@ -5,7 +5,54 @@ velocityDensityEstimator::velocityDensityEstimator(std::shared_ptr<kernelDensity
   KDE = kde;
 }
 
-double velocityDensityEstimator::countVelocitiDensityFromClusters(std::shared_ptr<cluster> clusters)
+double velocityDensityEstimator::countVelocitiDensityFromClusters(
+    std::vector<std::shared_ptr<cluster> > clusters)
 {
+  countTemporalWindowFromClusters(clusters);
 
+  double result = 0.0;
+
+  return result;
+}
+
+long velocityDensityEstimator::countTemporalWindowFromClusters(std::vector<std::shared_ptr<cluster> > clusters)
+{
+  long largestTimestamp, smallestTimestamp = largestTimestamp = clusters[0]->getTimestamp();
+  long clustersTimestamp;
+
+  for(std::shared_ptr<cluster> c : clusters)
+  {
+    clustersTimestamp = c->getTimestamp();
+
+    if(clustersTimestamp > largestTimestamp)
+      largestTimestamp = clustersTimestamp;
+
+    if(clustersTimestamp < smallestTimestamp)
+      smallestTimestamp = clustersTimestamp;
+  }
+
+  temporalWindow = largestTimestamp - smallestTimestamp;
+
+  return this->temporalWindow;
+}
+
+double velocityDensityEstimator::countForwardTimeSliceDensityFromClusters(
+    std::vector<std::shared_ptr<cluster> > clusters)
+{
+  double result = 0.0;
+
+  for(std::shared_ptr<cluster> c: clusters)
+  {
+
+  }
+
+  return result;
+}
+
+double velocityDensityEstimator::countSpatiotemporalKernelValueFromCluster(
+    std::shared_ptr<cluster> c)
+{
+  double result = 0.0;
+
+  return result;
 }
--- VDE/velocityDensityEstimator.h
+++ VDE/velocityDensityEstimator.h
@@ -7,20 +7,32 @@
 #include <memory>
 #include <vector>
 
-
 class velocityDensityEstimator
 {
   public:
+
     velocityDensityEstimator(std::shared_ptr<kernelDensityEstimator> kde);
 
-    double countVelocitiDensityFromClusters(std::shared_ptr<cluster> clusters);
+    double countVelocitiDensityFromClusters(std::vector<std::shared_ptr<cluster>> clusters);
 
   protected:
 
+    long temporalWindow;
+
     std::shared_ptr<kernelDensityEstimator> KDE;
 
-    double countForwardTimeSliceDensityFromClusters(std::shared_ptr<cluster> clusters);
-    double countReverseTimeSliceDensityFromClusters(std::shared_ptr<cluster> clusters);
+    long countTemporalWindowFromClusters(
+      std::vector<std::shared_ptr<cluster>> clusters
+    );
+    double countForwardTimeSliceDensityFromClusters(
+      std::vector<std::shared_ptr<cluster>> clusters
+    );
+      double countSpatiotemporalKernelValueFromCluster(
+        std::shared_ptr<cluster> c
+      );
+    double countReverseTimeSliceDensityFromClusters(
+      std::vector<std::shared_ptr<cluster>> clusters
+    );
 };
 
 #endif // VELOCITYDENSITYESTIMATOR_H
