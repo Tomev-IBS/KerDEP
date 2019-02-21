7d7f72be5249f725a90c3283efae5afbb2f627f5
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-02-21 21:56:05 +0100

    Exp51 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1475,7 +1475,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       qApp->processEvents();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 50\\";
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 51\\";
       //QString dirPath = "D:\\Dysk Google\\Badania\\test\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
