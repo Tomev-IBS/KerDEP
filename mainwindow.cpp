45d9caf2cc9b08e75b6e76478133f8b083ae27d3
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-03-31 11:53:50 +0200

    Re exp125 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2378,7 +2378,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       qApp->processEvents();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 124n\\";
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 125n\\";
       //QString dirPath = "D:\\Dysk Google\\Badania\\test\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
