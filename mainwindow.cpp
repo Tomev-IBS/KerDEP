1fb0b6508abad81f9d1a856247c126feedcb1cdd
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-04-06 09:40:09 +0200

    Exp145 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2372,7 +2372,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       qApp->processEvents();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 144\\";
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 145\\";
       //QString dirPath = "D:\\Dysk Google\\Badania\\test\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
