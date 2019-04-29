2291a1bb392c392427c712ddbdc2d0acd52ffd57
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-04-29 07:45:21 +0200

    Exp160 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2373,7 +2373,7 @@ void MainWindow::on_pushButton_start_clicked()
           ->setText("eta = " + formatNumberForDisplay(DESDAAlgorithm.getStationarityTestValue()));
 
       xTextLabel
-          ->setText("x = 12");
+          ->setText("x = 32");
 
       _lastSigmoidallyEnhancedPlotY.clear();
       lastKDEValues.clear();
@@ -2395,7 +2395,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       qApp->processEvents();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 159\\";
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 160\\";
       //QString dirPath = "D:\\Dysk Google\\Badania\\test\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
