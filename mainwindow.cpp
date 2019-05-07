96995f68042ef957b81062d1738fb518e15bac0b
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-05-07 05:49:13 +0200

    Exp169 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1200,7 +1200,7 @@ void MainWindow::on_pushButton_start_clicked()
 
   int stepsNumber = ui->lineEdit_iterationsNumber->text().toInt();
   int medoidsNumber = 50;
-  int l = 1024;
+  int l = 8;
 
   groupingThread gt(&storedMedoids, parser);
 
@@ -2393,7 +2393,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       qApp->processEvents();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 167\\";
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 169\\";
       //QString dirPath = "D:\\Dysk Google\\Badania\\test\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
