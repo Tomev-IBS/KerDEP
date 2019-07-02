d7e9cd2f25823921e52d7187907143768e3650f9
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-07-02 20:27:51 +0200

    Exp207 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1200,7 +1200,7 @@ void MainWindow::on_pushButton_start_clicked()
 
   int stepsNumber = ui->lineEdit_iterationsNumber->text().toInt();
   int medoidsNumber = 50;
-  int l = 4;
+  int l = 8;
 
   groupingThread gt(&storedMedoids, parser);
 
@@ -1389,7 +1389,7 @@ void MainWindow::on_pushButton_start_clicked()
   wTextLabel->position->setType(QCPItemPosition::ptAxisRectRatio);
   wTextLabel->position->setCoords(horizontalOffset, verticalOffset); // place position at center/top of axis rect
   wTextLabel->setFont(QFont(font().family(), fontSize)); // make font a bit larger
-  wTextLabel->setText("w_a = 0.99, w_EmE = " + QString::number(DESDAAlgorithm.w_E));
+  wTextLabel->setText("w_a = 0.98, w_EmE = " + QString::number(DESDAAlgorithm.w_E));
 
   verticalOffset += verticalStep;
 
@@ -2077,7 +2077,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       qApp->processEvents();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 206 (w_0.98, m_E=m_Eta=500, x = 4)\\";
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 207 (w_a = 0.98, m_E=m_Eta=500, x = 8)\\";
       //QString dirPath = "D:\\Dysk Google\\Badania\\test\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
