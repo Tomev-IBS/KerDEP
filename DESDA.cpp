2891c0333bbdc3e8fa3f9f2738c51d93eae68229
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-07-06 16:08:40 +0200

    Fixed mE=mEta=1000 size.

--- DESDA.cpp
+++ DESDA.cpp
@@ -35,7 +35,7 @@ DESDA::DESDA(std::shared_ptr<kernelDensityEstimator> estimator,
 
   _samplingAlgorithm->changeReservoirMaxSize(_maxM);
 
-  _mE = mE;
+  _mE = _maxM;
   _m = _maxM;
 
   _kpssM = _mE;// 2;
