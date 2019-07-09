6a95c1a9ff6aa2eef99385796a82f32ed58e769a
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-07-09 08:35:19 +0200

    Exp215 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2077,7 +2077,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       qApp->processEvents();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 214 (w_EmE = 0.96, m_E=m_Eta=1000)\\";
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 215 (w_EmE = 0.95, m_E=m_Eta=1000)\\";
       //QString dirPath = "D:\\Dysk Google\\Badania\\test\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
