93fb915a53dd8013d9a14a4ee6921e3339bee85e
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-07-24 21:10:46 +0200

    Exp260 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2101,7 +2101,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       ui->lineEdit_distributionProgression->text();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 259 ("
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 260 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", Lambda = " + QString::number(DESDAAlgorithm._lambda)+")\\";
       /*/
