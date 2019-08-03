daa2d98090f422025f455a9d6bed2118f1e1ed9b
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-08-03 09:58:51 +0200

    Exp279.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2104,7 +2104,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       ui->lineEdit_distributionProgression->text();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 278 ("
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 279 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", delta = " + QString::number(DESDAAlgorithm.delta)+")\\";
       /*/
