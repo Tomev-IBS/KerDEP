21d5ebf7ba48fafb639c8beff45e6a1d92855b13
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-08-10 15:36:13 +0200

    Exp283 setup.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2104,7 +2104,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       ui->lineEdit_distributionProgression->text();
 
-      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 282 ("
+      QString dirPath = "D:\\Dysk Google\\TR Badania\\Eksperyment 283 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", delta = " + QString::number(DESDAAlgorithm.delta)+")\\";
       /*/
