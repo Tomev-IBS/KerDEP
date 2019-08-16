e367bd31f278ea5618092d079450c17ee2afe6f4
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-08-16 11:06:08 +0200

    Exp289 fix.

--- DESDA.h
+++ DESDA.h
@@ -45,7 +45,7 @@ class DESDA
     double _u_i = 0.0;
     std::vector<double> _selectedVValues = {};
 
-    double delta = 0.10 gamma = 50000.0;
+    double delta = 0.0, gamma = 50000.0;
     double w_E = 0.98; // w_EmE
     double avg = 0;
     double _stDev = 1;
