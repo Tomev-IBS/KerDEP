f3b22721c27210ed5c21fa1356c833eb01fba2e4
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-10-13 08:59:23 +0200

    Exp339, take 2.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2108,7 +2108,7 @@ void MainWindow::on_pushButton_start_clicked()
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
       //QString googleDriveDir = "C:\\Users\\rybot\\Dysk Google\\"; // Work
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 341 ("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 339 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", delta eq2, random deletion)\\";
 
