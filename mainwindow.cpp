3b5ae9f7586cf8b03449be2bf3b45a59d0cb5115
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-12-13 07:20:42 +0100

    Real return to sliding window version.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2108,10 +2108,9 @@ void MainWindow::on_pushButton_start_clicked()
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
       //QString googleDriveDir = "C:\\Users\\rybot\\Dysk Google\\"; // Work
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 348 ("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 356 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
-                        ", new w)\\";
-
+                        ", new m)\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
 
