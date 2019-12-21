b62cc46c57b6a40667f4645edfadc2614ace0792
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-12-21 14:23:25 +0100

    Exp368, lambda=1600, v=0.005.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2108,7 +2108,7 @@ void MainWindow::on_pushButton_start_clicked()
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
       //QString googleDriveDir = "C:\\Users\\rybot\\Dysk Google\\"; // Work
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 367("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 368("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", new m, lambda = " + QString::number(lambda) +")\\";
 
