d6657c2f88e5fc415eec5c67dd86e0977446ae82
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-03-11 07:40:24 +0100

    Exp456, v=0.0001.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1303,7 +1303,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 455 ("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 456 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", r = " + QString::number(DESDAAlgorithm._r) +
                         ")\\";
