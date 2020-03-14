c0ec0fc7288d1df07caaacca5545fc25faf6c693
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-03-14 11:49:30 +0100

    Exp465. kpssm = 200.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1299,10 +1299,10 @@ void MainWindow::on_pushButton_start_clicked()
 
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 464 ("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 465 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", r = " + QString::number(DESDAAlgorithm._r) +
-                        ", m_KPSS = 1000)\\";
+                        ", m_KPSS = 200)\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
 
