5c79361efe0ffddfb721ef7872d280ab9091db16
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-03-15 11:27:25 +0100

    Exp485. Lowered kpss=500.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1181,7 +1181,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 484 ("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 485 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", uaktualniony wzór 36,  mmin=100, mkpss=1000)\\";
 
