0ad421ee9c54c4fb0ab7ca99273031a70f0c1f88
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-03-17 14:55:25 +0100

    Exp492.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1182,9 +1182,9 @@ void MainWindow::on_pushButton_start_clicked()
 
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 491 ("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 492 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
-                        ", mmin=mkpss=1000, 0.9 h)\\";
+                        ", mmin=100, mkpss=1000, 0.9 h)\\";
 
       if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
 
