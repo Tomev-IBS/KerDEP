d6f02b65f67b67a45fbbf3bed4a15db03c30a89f
trybotycki <t.rybotycki@dpcpolska.pl>
2020-03-20 12:28:50 +0100

    Exp507.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1190,7 +1190,7 @@ void MainWindow::on_pushButton_start_clicked()
 
       QString googleDriveDir = "D:\\Dysk Google\\"; // Home
 
-      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 506 ("
+      QString dirPath = googleDriveDir + "TR Badania\\Eksperyment 507 ("
                         "v = " + ui->lineEdit_distributionProgression->text() +
                         ", testy nowego użycia pochodnej, iw=10)\\";
 
