e1138b8c5d4dbeef85615391ee381272a92c2295
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-04 13:23:21 +0200

    Exp576.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -888,7 +888,7 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "575";
+  QString expNum = "576";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, 565, but seed=" + seedString;
   screenGenerationFrequency = 10;
