369484b6696d6936a19f953dd5b0d1f3970972f7
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-05 13:43:01 +0200

    Exp595.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -896,10 +896,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "594";
+  QString expNum = "595";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0.01, beta0=" +
-      QString::number(DESDAAlgorithm._beta0) + ",sz196";
+      QString::number(DESDAAlgorithm._beta0) + ",sz197";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Dysk Google\\"; // Home
