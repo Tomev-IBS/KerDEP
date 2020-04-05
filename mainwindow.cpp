d01249fe440d8dfda83e9e2d46224856362b34e5
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-05 13:47:24 +0200

    Exp598. Changing beta0.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -896,10 +896,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "597";
+  QString expNum = "598";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0.01, beta0=" +
-      QString::number(DESDAAlgorithm._beta0) + ",sz261";
+      QString::number(DESDAAlgorithm._beta0) + ",sz262";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Dysk Google\\"; // Home
