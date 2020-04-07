53435842c8e8b2c6d4593542b5a48ced51e62021
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-07 21:50:13 +0200

    Changed pc in exp612.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,7 +899,7 @@ void MainWindow::on_pushButton_start_clicked()
   QString expNum = "612";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0.01, beta0=" +
-      QString::number(DESDAAlgorithm._beta0) + ",sz022";
+      QString::number(DESDAAlgorithm._beta0) + ",sz129";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Dysk Google\\"; // Home
