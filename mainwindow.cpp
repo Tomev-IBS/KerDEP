2ca6971928407cc4be5679de1a3d5511bec936c3
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-09 21:40:42 +0200

    Exp634, with jumps.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -896,10 +896,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "632";
+  QString expNum = "634";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, skok, beta0=" +
-      QString::number(DESDAAlgorithm._beta0) + ",sz195";
+      QString::number(DESDAAlgorithm._beta0) + ",sz197";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Dysk Google\\"; // Home
