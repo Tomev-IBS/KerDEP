3eab002348a3d364e10bdaac2f9e8a28644032ff
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-16 23:52:02 +0200

    Exp684. Checking mMin on v=0.1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -896,10 +896,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "683";
+  QString expNum = "684";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0.1, beta0=0.7, mMin=" + QString::number(DESDAAlgorithm._minM) +
-                    ",sz022";
+                    ",sz129";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Dysk Google\\"; // Home
