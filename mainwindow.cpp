ed4df137ce8648696f4ac55d8953ad9e7c2fdbfb
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-17 17:36:50 +0200

    Exp693. ReExps with beta0=(52a).

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -896,9 +896,9 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "692";
+  QString expNum = "693";
   this->setWindowTitle("Experiment #" + expNum);
-  QString expDesc = "reservoir, v=0.1, beta0=(52a), sz129";
+  QString expDesc = "reservoir, v=0.05, beta0=(52a), sz195";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Dysk Google\\"; // Home
