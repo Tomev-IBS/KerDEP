585968404746eafae2dc06eb0e04aa031e30f830
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-23 11:35:02 +0200

    mMin experiments. Exp724.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -896,10 +896,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "723";
+  QString expNum = "724";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=1, beta0=0.66..., mMin=" +
-                      QString::number(DESDAAlgorithm._minM) +", sz196";
+                      QString::number(DESDAAlgorithm._minM) +", sz197";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Dysk Google\\"; // Home
