2f61a0e46e2bca9c2bedf4943ae3f1d12f8f76c7
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-24 19:58:24 +0200

    mMin experiments. Exp745.

--- Reservoir_sampling/progressivedistributiondatareader.cpp
+++ Reservoir_sampling/progressivedistributiondatareader.cpp
@@ -46,6 +46,7 @@ void progressiveDistributionDataReader::getNextRawDatum(void *target)
     */
 
 
+    /*
     switch(_currentIteration - 1){ // For exps with seed, remove later
       case 0:
         progressionSize = 0;
@@ -57,9 +58,9 @@ void progressiveDistributionDataReader::getNextRawDatum(void *target)
         progressionSize = 0;
         break;
     }
+    */
 
 
-    /*
     switch(_currentIteration - 1){ // For exps with seed, remove later
       case 0:
         progressionSize = 0;
@@ -83,7 +84,6 @@ void progressiveDistributionDataReader::getNextRawDatum(void *target)
         progressionSize = 0;
         break;
     }
-    */
 
     if(_currentIteration > _delay){
       if(_shouldJump && ! _hasJumped){
