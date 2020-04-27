c678cab66609dceb39afd75847b36933148d99cd
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-27 02:12:39 +0200

    sgmKPSS researches. Exp758.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,9 +899,9 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "757";
+  QString expNum = "758";
   this->setWindowTitle("Experiment #" + expNum);
-  QString expDesc = "reservoir, v=0.01, beta0=(55a), 15 percent, sz260";
+  QString expDesc = "reservoir, v=0.1, beta0=(55a), 15 percent, sz261";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
