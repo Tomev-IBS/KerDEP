6266fe26d9e1ab1a2a7d1f957494b7f83bd36dd4
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-02 15:26:01 +0200

    mMin experiments. Exp805.

--- DESDA.cpp
+++ DESDA.cpp
@@ -35,7 +35,8 @@ DESDA::DESDA(std::shared_ptr<kernelDensityEstimator> estimator,
   _m = _maxM;
   _mA = _maxM / 10; // For avg max |a| calculation
 
-  _minM = _maxM / 10;
+  //_minM = _maxM / 10;
+  _minM = 10;
   _kpssM = _maxM;
 
   _sgmKPSS = -1;
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,9 +899,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "804";
+  QString expNum = "805";
   this->setWindowTitle("Experiment #" + expNum);
-  QString expDesc = "reservoir, v=0-1-0-1, beta0=(55a), 20 percent, sz474";
+  QString expDesc = "reservoir, v=0-1-0-1, mMin=" + QString::number(DESDAAlgorithm._minM) +
+                    ", sz474";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
