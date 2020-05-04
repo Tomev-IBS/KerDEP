19945eec84b6f7224dcb1b9a6b2f2b9a44d56507
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-04 16:23:14 +0200

    Work in progress on mMin parameter value. Exp830.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,10 +899,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "829";
+  QString expNum = "830";
   this->setWindowTitle("Experiment #" + expNum);
-  QString expDesc = "reservoir, v=0.01, mMin=" + QString::number(DESDAAlgorithm._minM) +
-                    ", sz130";
+  QString expDesc = "reservoir, v=0.1, mMin=" + QString::number(DESDAAlgorithm._minM) +
+                    ", sz195";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
