d741d8a7d89315ef211dda2ac7506f6e62844458
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-05 16:46:43 +0200

    m0 experiments. Exp853.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -898,10 +898,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "852";
+  QString expNum = "853";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0-1-0-1, m0 =" + QString::number(DESDAAlgorithm._maxM) +
-                    ", sz197";
+                    ", sz260";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
