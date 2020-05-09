ce94bc033d6d28305c3e2f1d031f285719b41875
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-09 16:31:14 +0200

    m0 and mMin experiments after afixing mKPSS. Exp891.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -898,10 +898,10 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "890";
+  QString expNum = "891";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0.001, m0="+QString::number(sampleSize)+
-                    ", mMin="+ QString::number(DESDAAlgorithm._minM)+", sz197";
+                    ", mMin="+ QString::number(DESDAAlgorithm._minM)+", sz260";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
