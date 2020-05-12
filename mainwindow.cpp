222aefaaf95155fd7bdfd36808c003317c1a37ef
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-12 18:09:02 +0200

    mKPSS and Ziegler-Nichols experiments. Exp915.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -898,11 +898,11 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "914";
+  QString expNum = "915";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0-1-0-1, mKPSS="
                     + QString::number(DESDAAlgorithm._kpssM) +
-                    ", 17 percent, sz477";
+                    ", 18 percent, sz130";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
