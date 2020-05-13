2681bf48f7d3c9b5b56088ab2ce3592fb55e15d6
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-13 19:33:35 +0200

    mMin and m0 exps with afixed mKPSS. Exp918.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -898,11 +898,12 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "917";
+  QString expNum = "918";
   this->setWindowTitle("Experiment #" + expNum);
-  QString expDesc = "reservoir, v=0-1-0-1, mKPSS="
-                    + QString::number(DESDAAlgorithm._kpssM) +
-                    ", 20 percent, sz260";
+  QString expDesc = "reservoir, v=0.01, m0="
+                    + QString::number(DESDAAlgorithm._maxM) +
+                    "mMin=" + QString::number(DESDAAlgorithm._minM) +
+                    ", 13 percent, sz261";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
