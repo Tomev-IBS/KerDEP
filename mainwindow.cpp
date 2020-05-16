339937058a38aeca44222d5f6c39404e0ecebd8c
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-16 16:23:37 +0200

    Percentage check for mKPSS=500 with fixed m0. Exp942.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,12 +899,12 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "941";
+  QString expNum = "942";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0-1-0-1, mKPSS="
                     + QString::number(DESDAAlgorithm._kpssM) +
                     ", " + QString::number(DESDAAlgorithm._sgmKPSSPercent) +
-                    " percent, sz195";
+                    " percent, sz196";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
