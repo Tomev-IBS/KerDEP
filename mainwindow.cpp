91ac76508515938eec056ad63b2cffdbe1bffd36
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-18 11:53:59 +0200

    New strategy tests with percent finding experiments. Exp953.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,12 +899,12 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "952";
+  QString expNum = "953";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0-1-0-1, mKPSS="
                     + QString::number(DESDAAlgorithm._kpssM) +
                     ", " + QString::number(DESDAAlgorithm._sgmKPSSPercent) +
-                    " percent, sz003";
+                    " percent, sz022";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
