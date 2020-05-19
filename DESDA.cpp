a4926e33d25bd33eed521c0e83d0a4fc4ecff254
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-19 13:05:30 +0200

    New strategy tests with percent finding experiments. Exp961.

--- DESDA.cpp
+++ DESDA.cpp
@@ -36,7 +36,7 @@ DESDA::DESDA(std::shared_ptr<kernelDensityEstimator> estimator,
   _kpssM = 500; // This is independent of maxM.
 
   _sgmKPSS = -1;
-  _sgmKPSSPercent = 16;
+  _sgmKPSSPercent = 31;
   _stepNumber = 1;
 
   stationarityTest.reset(new KPSSStationarityTest(_kpssM));
