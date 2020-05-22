1225b0d6380246b0baf5e376655a3ad8201d4a05
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-05-22 12:57:26 +0200

    Min m and Max m experiments with DESDA reservoir without tail. Exp992.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,12 +899,12 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "991";
+  QString expNum = "992";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0, m0="
                     + QString::number(DESDAAlgorithm._maxM) +
                     ", mMin=" + QString::number(DESDAAlgorithm._minM) +
-                    ", sz003";
+                    ", sz022";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
