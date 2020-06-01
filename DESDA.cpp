377f0cd9b925e93ecb921be050b4f4dec14398db
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-06-01 15:25:10 +0200

    Min m and Max m experiments with DESDA reservoir without tail. Exp1052.

--- DESDA.cpp
+++ DESDA.cpp
@@ -32,7 +32,7 @@ DESDA::DESDA(std::shared_ptr<kernelDensityEstimator> estimator,
   _m = _maxM;
   _mA = _maxM / 10; // For avg max |a| calculation
 
-  _minM = 500; // 50, 100, 200, 500
+  _minM = 100; // 50, 100, 200, 500
   _kpssM = 500; // This is independent of maxM.
 
   _sgmKPSS = -1;
