2959354cd0c21165f918b8798e879aadf4f03505
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-06-08 00:23:28 +0200

    Optimal m0 research. Experiment 1089.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,11 +899,11 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "1077";
+  QString expNum = "1089";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, v=0-1-0-1, m0= "+ QString::number(DESDAAlgorithm._maxM) +
                     ", mMin=" + QString::number(DESDAAlgorithm._minM) +
-                    ", sz003";
+                    ", sz022";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
