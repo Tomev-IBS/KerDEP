ec89c34bf2b0aa47848094debc581171a662d551
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-06-14 23:23:03 +0200

    Finding optimal parameters for multimodal distributions. Experiment 1149.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -899,11 +899,11 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "1147";
+  QString expNum = "1149";
   this->setWindowTitle("Experiment #" + expNum);
-  QString expDesc = "reservoir, plugin 2, N(0,1)N(5,1), mKPSS=750, v=tor, m0= "+ QString::number(DESDAAlgorithm._maxM) +
+  QString expDesc = "reservoir, plugin 2, N(0,1)N(5,1), v=tor, m0= "+ QString::number(DESDAAlgorithm._maxM) +
                     ", mMin=" + QString::number(DESDAAlgorithm._minM) +
-                    ", sz263";
+                    ", sz423";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
