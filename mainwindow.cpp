0952e7f820d0039b1bc237c603b6af85a2af9a88
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-06-19 23:12:33 +0200

    Finding optimal mKPSS parameter for multimodal distributions. Experiment 1177.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -905,13 +905,13 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "1176";
+  QString expNum = "1177";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, plugin " + QString::number(pluginRank) +
                     ", N(0,1), v=tor, m0= "+ QString::number(DESDAAlgorithm._maxM) +
                     ", mMin=" + QString::number(DESDAAlgorithm._minM) +
                     ", mKPSS=" + QString::number(DESDAAlgorithm._kpssM) + // This can just be commented out.
-                    ", sz002";
+                    ", sz003";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
