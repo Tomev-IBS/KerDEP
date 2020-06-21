396b5dda73821bf720d1be4f765e025949815d5a
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-06-21 12:42:21 +0200

    Finding optimal mKPSS parameter for multimodal distributions. Experiment 1200.

--- Reservoir_sampling/progressivedistributiondatareader.cpp
+++ Reservoir_sampling/progressivedistributiondatareader.cpp
@@ -30,7 +30,7 @@ void progressiveDistributionDataReader::getNextRawDatum(void *target)
     int bimodalMean = 5;
     int trimodalMean = -5;
 
-    /*
+    //*
     // Bimodal selection scheme.
     if((_currentIteration - 1) % 10 == 1 || (_currentIteration - 1) % 10 == 4 ||
        (_currentIteration - 1) % 10 == 7 || (_currentIteration - 1) % 10 == 9){
@@ -38,7 +38,7 @@ void progressiveDistributionDataReader::getNextRawDatum(void *target)
     }
     //*/
 
-    //*
+    /*
     // Trimodal selection scheme.
     if((_currentIteration - 1) % 10 == 1 || (_currentIteration - 1) % 10 == 4 ||
        (_currentIteration - 1) % 10 == 7){
@@ -53,7 +53,7 @@ void progressiveDistributionDataReader::getNextRawDatum(void *target)
 
     // 26 III 2020 article formula
     // Stops at 0.2 + 30 + 3 + 1 = 34.2 without offset. Set maxX = 40.
-    //*
+    /*
     switch(_currentIteration - 1){ // For exps with seed, remove later
       case 0:
         progressionSize = 0.0001;
@@ -87,7 +87,7 @@ void progressiveDistributionDataReader::getNextRawDatum(void *target)
     }
     /**/
 
-    /*
+    //*
     switch(_currentIteration - 1){ // For exps with seed, remove later
       case 0:
         progressionSize = 0;
