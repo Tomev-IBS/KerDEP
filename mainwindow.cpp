97ecd137832b8a0cc01d9992b3f273e70b90eb53
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-06-25 11:14:01 +0200

    Finding optimal m0 parameter for multimodal distributions. Experiment 1239, name fix.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -911,7 +911,7 @@ void MainWindow::on_pushButton_start_clicked()
                     ", N(-5,1)N(0,1)N(5,1), v=0-1-0-1, m0="+ QString::number(DESDAAlgorithm._maxM) +
                     ", mMin=" + QString::number(DESDAAlgorithm._minM) +
                     //", mKPSS=" + QString::number(DESDAAlgorithm._kpssM) + // This can just be commented out.
-                    ", sz197";
+                    ", sz022";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
