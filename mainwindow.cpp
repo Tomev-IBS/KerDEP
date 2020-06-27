691d0b0503db4ab0f9dd7780bd3d1d08e73dc09b
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-06-27 19:30:15 +0200

    Finding optimal m0 parameter for multimodal distributions. Experiment 1265.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -905,13 +905,13 @@ void MainWindow::on_pushButton_start_clicked()
   );
 
 
-  QString expNum = "1264";
+  QString expNum = "1265";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "reservoir, plugin " + QString::number(pluginRank) +
                     ", 0,2N(-5,1)0,4N(0,1)0,4N(5,1), v=0-1-0-1, m0="+ QString::number(DESDAAlgorithm._maxM) +
                     ", mMin=" + QString::number(DESDAAlgorithm._minM) +
                     //", mKPSS=" + QString::number(DESDAAlgorithm._kpssM) + // This can just be commented out.
-                    ", sz421";
+                    ", sz422";
   screenGenerationFrequency = 10;
 
   //QString driveDir = "D:\\Test\\"; // Home
