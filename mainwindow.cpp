d48fdd5fe17fa041b125f525547313785bd600f7
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-09-01 19:55:07 +0200

    Fixed problem with the KDE. Exp1294.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1268,12 +1268,12 @@ void MainWindow::on_pushButton_clicked()
 {
   qDebug() << "2D Experiment start.";
 
-  screenGenerationFrequency = 2;
+  screenGenerationFrequency = 100;
   int seed = ui->lineEdit_seed->text().toInt();
 
   // Prepare image location.
-  QString expNum = "1293 (2D)";
-  QString expDesc = "iw=1000, v=tor na x_1, sz001";
+  QString expNum = "1294 (2D)";
+  QString expDesc = "iw=+" + QString::number(screenGenerationFrequency) + ", v=tor na x_1, sz002";
   QString driveDir = "\\\\beabourg\\private\\"; // WIT PCs
   //QString driveDir = "D:\\Test\\"; // Home
   //QString driveDir = "d:\\OneDrive - Instytut Badań Systemowych Polskiej Akademii Nauk\\";
