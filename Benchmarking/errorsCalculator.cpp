0e653e0460b2f71c0b1b5b50781119e18f348bc9
Tomasz Rybotycki <rybotycki.tomasz+github@gmail.com>
2020-09-27 20:09:18 +0200

    Initial version of finished errors calculator.

--- Benchmarking/errorsCalculator.cpp
+++ Benchmarking/errorsCalculator.cpp
@@ -2,6 +2,16 @@
 
 #include <math.h>
 
+ErrorsCalculator::ErrorsCalculator(std::vector<double> *model_function_values,
+                                   std::vector<double> *estimator_values,
+                                   std::vector<
+                                       std::vector<double>> *error_domain,
+                                   double *domain_quantity)
+ : model_function_values_(model_function_values),
+   estimator_values_(estimator_values), error_domain_(error_domain),
+   domain_quantity_(domain_quantity)
+{}
+
 double ErrorsCalculator::CalculateL1Error() {
   /** This method calculates l1 error between estimated values and model values.
    * @brief This method calculates l1 error between estimated values and model values.
@@ -59,7 +69,24 @@ double ErrorsCalculator::CalculateSupError() {
 }
 
 double ErrorsCalculator::CalculateModError() {
-  return 0;
+  /** Calculates mod error between model and estimated values.
+   * @brief Calculates mod error between model and estimated values.
+   */
+  // Ensure, that both estimated and model values have the same dimension
+  // as domain. In this case return -1.
+  if( error_domain_->size() != estimator_values_->size() ||
+      error_domain_->size() != model_function_values_->size()){
+    return -1;
+  }
+  // Find greatest value of model and estimated values.
+  auto max_model_value_index = FindMaxValueIndex(*model_function_values_);
+  auto max_estimator_value_index =
+      FindMaxValueIndex(*estimator_values_);
+  auto model_max_value_point = (*error_domain_)[max_model_value_index];
+  auto estimator_max_value_point = (*error_domain_)[max_estimator_value_index];
+  // Return distance between selected points.
+  return CalculateEuclideanDistance(model_max_value_point,
+                                    estimator_max_value_point);
 }
 
 double ErrorsCalculator::FindMaxValueIndex(const std::vector<double> &values) {
@@ -67,6 +94,42 @@ double ErrorsCalculator::FindMaxValueIndex(const std::vector<double> &values) {
    * @brief Finds the index of maximal value in given vector.
    */
    // If there are no values in the vector return -1;
-
+   if(values.empty()){
+     return -1;
+   }
    // Initialize index and maximal value with the first value of vector.
+   int max_value_index = 0;
+   double max_value = values[0];
+
+   // Check if there are no greater values then initialized one.
+   for(auto i = 0; i < values.size(); ++i){
+     if(max_value < values[i]){
+       max_value = values[i];
+       max_value_index = i;
+     }
+   }
+
+   // Return value found.
+   return max_value_index;
 }
+
+double ErrorsCalculator::CalculateEuclideanDistance(
+    const std::vector<double> &point_1, const std::vector<double> &point_2) {
+  /** Calculates euclidean distance between two given points. For this method
+   * to work correctly both points have to have same dimension.
+   * @brief Calculates euclidean distance between two given points.
+   */
+  // Ensure, that dimension of both points i equal. If not return -1.
+  if(point_1.size() != point_2.size()){
+    return -1;
+  }
+  // Calculate sum of squares of differences between the points.
+  double sum = 0;
+  for(auto i = 0; i < point_2.size(); ++i){
+    sum += pow(point_1[i] - point_2[i], 2);
+  }
+  // Return square root of this sum (the euclidean distance).
+  return pow(sum, 0.5);
+}
+
+
--- Benchmarking/errorsCalculator.h
+++ Benchmarking/errorsCalculator.h
@@ -12,7 +12,10 @@ class ErrorsCalculator {
      * probability density functions.
      */
   public:
-    ErrorsCalculator();
+    ErrorsCalculator(std::vector<double> *model_function_values,
+                     std::vector<double> *estimator_values,
+                     std::vector<std::vector<double>> *error_domain,
+                     double *domain_quantity);
     double CalculateL1Error();
     double CalculateL2Error();
     double CalculateSupError();
@@ -20,11 +23,11 @@ class ErrorsCalculator {
   private:
     std::vector<double> *model_function_values_;
     std::vector<double> *estimator_values_;
-    std::vector<std::vector<double>> error_domain_;
+    std::vector<std::vector<double>> *error_domain_;
     double *domain_quantity_; // For one dimension it should be length, in two it should be area, and so...
 
     double FindMaxValueIndex(const std::vector<double> &values);
-    double CalculateEuclideanDistance(const std::vector<double> &point_1, const std::vector<double> &point2);
+    double CalculateEuclideanDistance(const std::vector<double> &point_1, const std::vector<double> &point_2);
 };
 
 #endif //KERDEP_ERRORSCALCULATOR_H
