90038c0aec86fb35c92d01348debb89e25ef125b
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-09-28 01:45:34 +0200

    Exp1346 with fixed errors calculation and initial steps, denser error domain.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1161,11 +1161,11 @@ void MainWindow::on_pushButton_clicked() {
   int m0 = ui->lineEdit_sampleSize->text().toInt();
 
   // Prepare image location.
-  QString expNum = "1345 (2D)";
+  QString expNum = "1346 (2D)";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc =
       "iw=" + QString::number(screen_generation_frequency_)
-      + ",poprawione błędy, 1m0, v=tor na x_1, m0=4k, mMin=400, sz129";
+      + ",poprawione błędy, 10m0, v=tor na x_1, m0=4k, mMin=400, sz263";
   //QString driveDir = "\\\\beabourg\\private\\"; // WIT PCs
   QString driveDir = "Y:\\"; // WIT PCs after update
   //QString driveDir = "D:\\Test\\"; // Home
