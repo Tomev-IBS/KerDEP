4e237b2f0f003651a8abdf91b764a840558b1e15
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-09-30 02:31:41 +0200

    Exp1348 with fixed errors calculation and initial steps, denser error domain. Last series on WIT.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1156,20 +1156,20 @@ void MainWindow::on_pushButton_start_clicked() {
 void MainWindow::on_pushButton_clicked() {
   log("2D Experiment start.");
 
-  screen_generation_frequency_ = 100;
+  screen_generation_frequency_ = 1;
   int seed = ui->lineEdit_seed->text().toInt();
   int m0 = ui->lineEdit_sampleSize->text().toInt();
 
   // Prepare image location.
-  QString expNum = "1346 (2D)";
+  QString expNum = "1348 (2D)";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc =
       "iw=" + QString::number(screen_generation_frequency_)
-      + ",poprawione błędy, 10m0, v=tor na x_1, m0=4k, mMin=400, sz263";
+      + ",poprawione błędy, 1m0, v=tor na x_1, m0=4k, mMin=400, sz001";
   //QString driveDir = "\\\\beabourg\\private\\"; // WIT PCs
-  QString driveDir = "Y:\\"; // WIT PCs after update
+  //QString driveDir = "Y:\\"; // WIT PCs after update
   //QString driveDir = "D:\\Test\\"; // Home
-  //QString driveDir = "d:\\OneDrive - Instytut Badań Systemowych Polskiej Akademii Nauk\\";
+  QString driveDir = "d:\\OneDrive - Instytut Badań Systemowych Polskiej Akademii Nauk\\";
   QString dirPath = driveDir + "TR Badania\\Eksperyment " + expNum + " ("
                     + expDesc + ")\\";
   if(!QDir(dirPath).exists()) QDir().mkdir(dirPath);
