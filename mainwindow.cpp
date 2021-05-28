67e36d99749b841d6926a880618705fe96771a33
Tomev <rybotycki.tomasz+github@gmail.com>
2021-05-28 07:24:00 +0200

    exp(1D): Prepare BOTH airports experiment.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1163,7 +1163,7 @@ void MainWindow::Run1DExperimentWithDESDA() {
   // std::string data_path = "k:\\Coding\\Python\\KerDEP_Data_Preparator\\AirQuality\\result.txt";
   // std::string data_path = "k:\\Coding\\Python\\KerDEP_Data_Preparator\\Cracow_Temp_2016\\result.txt";
   std::string data_path = "k:\\Coding\\Python\\KerDEP_Data_Preparator\\BikeSharing\\result.txt";
-  data_path = "y:\\Data\\MIA.csv";
+  data_path = "y:\\Data\\BOTH.txt";
 
   // reader_.reset(new TextDataReader("k:\\Coding\\Python\\KerDEP_Data_Preparator\\AirQuality\\result.txt"));
   reader_.reset(new TextDataReader(data_path));
@@ -1203,12 +1203,12 @@ void MainWindow::Run1DExperimentWithDESDA() {
       ui->lineEdit_rarity->text().toDouble(), newWeightB, pluginRank
                       );
 
-  QString expNum = "1533";
+  QString expNum = "1534";
   this->setWindowTitle("Experiment #" + expNum);
   QString expDesc = "DESDA, Plugin" + QString::number(pluginRank) +
                     ", EWR (in 100 minutes), m0=" + QString::number(DESDAAlgorithm._maxM) +
                     ", mMin=" + QString::number(DESDAAlgorithm._minM) +
-                    ", sz197";
+                    ", sz261";
 
   screen_generation_frequency_ = 10;
   bool compute_errors = false;
