a61f5a3ab9664b4b6f064992cab79cf26a84ecc8
Tomev <rybotycki.tomasz+github@gmail.com>
2023-01-06 18:25:00 +0100

    exp(review): Prepare m_0 experiments.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1346,7 +1346,7 @@ void MainWindow::Run1DExperimentWithDESDA() {
   derivative_estimator_.reset(GenerateKernelDensityEstimator(dimensionsNumber));
   enhanced_kde_.reset(GenerateKernelDensityEstimator(dimensionsNumber));
 
-  double evenDistributionsMultiplier = -1;  // 1 to standardowa "ścieżka zdrowia / życia"
+  double evenDistributionsMultiplier = 1;  // 1 to standardowa "ścieżka zdrowia / życia"
 
   std::shared_ptr<distribution>
       targetDistribution(GenerateTargetDistribution(&means_, &standard_deviations_, evenDistributionsMultiplier));
@@ -1392,8 +1392,8 @@ void MainWindow::Run1DExperimentWithDESDA() {
   //double p2 = 0.1;
 
   //QString expDesc = "assumed data stream,  sz221";
-  QString expDesc = "id=" + QString::number(screen_generation_frequency_) + ", splitting, v_1="+QString::number(evenDistributionsMultiplier)+"v_2)";
-  QString plot_description = "splitting, v_1="+QString::number(evenDistributionsMultiplier)+"v_2; 1D";
+  QString expDesc = "id=" + QString::number(screen_generation_frequency_) + ", assumed data stream, m_0=" + ui->lineEdit_sampleSize->text();
+  QString plot_description = "assumed data stream, m_0=" + ui->lineEdit_sampleSize->text() +"; 1D";
   QDate startDate(2019, 10, 1); // It's not used anyway.
   ui->checkBox_showEstimatedPlot->setChecked(true);
   //QString path_length = QString::number(2 + p2 * 4000 + 0 + 1 + 0 + 5);
@@ -1401,8 +1401,8 @@ void MainWindow::Run1DExperimentWithDESDA() {
   //*/
 
   int drawing_start_step = 0;
-  QString expNum = "R12";
-  QString pcName = "sz511";
+  QString expNum = "R18";
+  QString pcName = "sz517";
 
   expDesc += ", " + pcName;
 
