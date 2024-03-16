f1ace3401df1a6a299821fbc146cc3e1799c1a0a
Tomev <rybotycki.tomasz+github@gmail.com>
2024-03-16 05:06:46 +0100

    exp(WDE): Prepare trimodal WDE experiments.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -929,8 +929,8 @@ void MainWindow::on_pushButton_start_clicked() {
   //RunAccuracyExperiment();
 
   // Set target function
-  SetBimodalTargetFunction();
-  //SetTrimodalTargetFunction();
+  //SetBimodalTargetFunction();
+  SetTrimodalTargetFunction();
 
   // Set number of iterations
   this->ui->lineEdit_iterationsNumber->setText("10000");
@@ -938,7 +938,7 @@ void MainWindow::on_pushButton_start_clicked() {
   screen_generation_frequency_ = 1000;
   error_computation_frequency_ = 10;
   int n_seeds = 20;
-  stream_number = 13;
+  stream_number = 14;
 
   for(int seed = n_seeds - 19; seed < n_seeds + 1; ++seed){
   //for(int seed = n_seeds; seed > 0; --seed){  // Reversed loop for other experiments.
